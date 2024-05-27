baf70049e149cd85c3169b0ecf2207ed745a4c34
Francesco Moramarco <fm2g11@gmail.com>
2024-05-27 13:09:42 +0100

    fixes

--- progetti/news.js
+++ progetti/news.js
@@ -5,11 +5,5 @@ NEWS = [
         "preview": "images/news/san-felice.jpg",
         "content": "SURANO/SPONGANO - Dopo anni di abbandono, sono finalmente iniziati i lavori di restauro della Cappella rurale di San Felice, risalente al periodo compreso tra i secoli XVI e XVII, denominata anche “Cappella della Circoncisione” per la presenza di un rarissimo affresco raffigurante la scena della circoncisione del Cristo: una rinascita che mette fine alla lunga e tormentata storia di questo piccolo ma prezioso bene, che si incontra tra le campagne di Surano e Spongano in provincia di Lecce, e che rappresenta una traccia della devozione genuina che ha animato per secoli le piccole comunità rurali del Salento.",
         "link": "https://www.lecceprima.it/attualita/restauro-chiesetta-san-felice-surano-18-maggio-2023.html"
-    },
-    {
-        "title": "News 2",
-        "preview": "images/news/san-felice.jpg",
-        "content": "SURANO/SPONGANO - Dopo anni di abbandono, sono finalmente iniziati i lavori di restauro della Cappella rurale di San Felice, risalente al periodo compreso tra i secoli XVI e XVII, denominata anche “Cappella della Circoncisione” per la presenza di un rarissimo affresco raffigurante la scena della circoncisione del Cristo: una rinascita che mette fine alla lunga e tormentata storia di questo piccolo ma prezioso bene, che si incontra tra le campagne di Surano e Spongano in provincia di Lecce, e che rappresenta una traccia della devozione genuina che ha animato per secoli le piccole comunità rurali del Salento.",
-        "link": "https://www.lecceprima.it/attualita/restauro-chiesetta-san-felice-surano-18-maggio-2023.html"
     }
 ]
