e63c0930714a02892911fa2341d6d37143e800f3
Francesco Moramarco <fm2g11@gmail.com>
2024-11-12 23:39:34 +0000

    added new progetti pubblici

--- docs/progetti/progetti.js
+++ docs/progetti/progetti.js
@@ -1,7 +1,9 @@
 PROJECTS = {
     'progetti-pubblici': [
         'alloggio-anziani-calimera.json',
-        'scavi-archeologici-di-roca.json'
+        'scavi-archeologici-di-roca.json',
+        'parco-agricolo-dei-paduli.json',
+        'bocciodromo-castri.json'
     ],
     'progetti-privati': [
         'flat-renovation-londra.json',
