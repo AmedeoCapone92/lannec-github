fe65b2bd86bef66c94cadb26495547d1038f9c99
Francesco Moramarco <fm2g11@gmail.com>
2025-01-17 14:16:18 +0000

    fixes

--- lannec-django/lannec/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LannecConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lannec'
--- lannec-django/lannec/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import gara_detail
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path('gara/<int:id>/', gara_detail, name='gara_detail'),
-]
